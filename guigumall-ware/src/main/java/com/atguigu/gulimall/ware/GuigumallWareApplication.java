193ee3669117b749fbaabf0642b1aaee3dea4a46
huke <3572036312@qq.com>
2023-07-25 00:26:56 +0800

    项目初始结构创建

---
+++ guigumall-ware/src/main/java/com/atguigu/gulimall/ware/GuigumallWareApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuigumallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuigumallWareApplication.class, args);
+    }
+
+}
